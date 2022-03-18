f6339ffaa7edf9a9f108384ee59fdb04db5c9fa3
scott <scott.rachau@yahoo.com>
2022-03-18 11:12:50 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  randomText = lorem.sentence();
+  enteredText = "";
+  
+  onInput(value: any) {
+    console.log(value);
+    this.enteredText = value;
+  
+  }
+}
